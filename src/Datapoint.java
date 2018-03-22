59373d3ff7cb0e90f49281061d45ec85fc775ce5
Niels Hvid <nielshvid@gmail.com>
2018-03-22 14:14:27 +0100

    Started WiFi fingerprinting project

--- src/Datapoint.java
+++ src/Datapoint.java
@@ -1,6 +1,17 @@
-public class Datapoint {
-    int x;
-    int y;
+import java.util.ArrayList;
+import java.util.List;
 
-    List<Measurement>
+public class Datapoint {
+    Position position;
+
+    private List<Datareading> reading;
+
+    Datapoint(Position _position){
+        position = _position;
+        reading = new ArrayList<>();
+    }
+
+    public void addDataReading(String addr, int SignalStrength){
+        reading.add(new Datareading(addr, SignalStrength))
+    }
 }
